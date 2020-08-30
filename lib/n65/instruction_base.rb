963dab73cc9b561eaaf515d7d7ff47521abf5329
Saf <saf@irkenkitties.com>
2020-08-30 12:36:50 -0700

    Linted instruction_base.rb

--- lib/n65/instruction_base.rb
+++ lib/n65/instruction_base.rb
@@ -1,29 +1,17 @@
+# frozen_string_literal: true
 
 module N65
-
   class InstructionBase
-
-
-    #####
-    ##  Sort of a "pure virtual" class method, not really tho.
-    def self.parse(line)
-      fail(NotImplementedError, "#{self.class.name} must implement self.parse")
+    def self.parse(_line)
+      raise(NotImplementedError, "#{self.class.name} must implement self.parse")
     end
 
-
-    ####
-    ##  Does this instruction have unresolved symbols?
     def unresolved_symbols?
       false
     end
 
-
-    ####
-    ##  Another method subclasses will be expected to implement
-    def exec(assembler)
-      fail(NotImplementedError, "#{self.class.name} must implement exec")
+    def exec(_assembler)
+      raise(NotImplementedError, "#{self.class.name} must implement exec")
     end
-
   end
-
 end
